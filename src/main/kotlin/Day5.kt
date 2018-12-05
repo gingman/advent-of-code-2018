d6aa615174cc5db7f9662c696d31b78fb141240e
Simon Gingras <thegingman@gmail.com>
2018-12-05 16:38:39 -0500

    Trim input

--- src/main/kotlin/Day5.kt
+++ src/main/kotlin/Day5.kt
@@ -2,7 +2,7 @@ import java.io.File
 
 fun main(args : Array<String>) {
     val resource = Int::class.java.getResource("day5.input.txt")
-    val input = File(resource.toURI()).readText()
+    val input = File(resource.toURI()).readText().trim()
     println("Part 1: " + Day5.part1(input))
     println("Part 2: " + Day5.part2(input))
 }
@@ -15,22 +15,23 @@ object Day5 {
         return units.count()
     }
 
-    private fun removeAdjacentUnit(input: String, regexp: Regex): String {
-        if (regexp.containsMatchIn(input)) {
-            return removeAdjacentUnit(regexp.replaceFirst(input, ""), regexp)
-        }
-        return input
-    }
-
     fun part2(input: String): Int {
         val regexp = getAdjacentUnitRegex()
         val unitsLength = mutableListOf<Int>()
+
         ('a'..'z').forEach {
             val inputWithoutChar = Regex("$it", RegexOption.IGNORE_CASE).replace(input, "")
             unitsLength.add(removeAdjacentUnit(inputWithoutChar, regexp).count())
         }
 
         return unitsLength.min()!!
+    }
+
+    private fun removeAdjacentUnit(input: String, regexp: Regex): String {
+        if (regexp.containsMatchIn(input)) {
+            return removeAdjacentUnit(regexp.replaceFirst(input, ""), regexp)
+        }
+        return input
     }
 
     private fun getAdjacentUnitRegex(): Regex {
